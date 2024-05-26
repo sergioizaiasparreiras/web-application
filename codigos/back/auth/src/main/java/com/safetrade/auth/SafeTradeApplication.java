25e362b9cec04d8b9b1810dd859f88c091d7f3ab
sergioizaiasparreiras <sergioparreiras09@gmail.com>
2024-05-26 20:32:09 -0300

    feature:backend-api

---
+++ codigos/back/auth/src/main/java/com/safetrade/auth/SafeTradeApplication.java
@@ -0,0 +1,13 @@
+package com.safetrade.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeTradeApplication.class, args);
+	}
+
+}
---
+++ codigos/back/auth/src/main/java/com/safetrade/auth/repository/ProdutoRepository.java
@@ -0,0 +1,79 @@
+package com.safetrade.auth.repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import com.safetrade.auth.model.Produto;
+
+
+
+public class ProdutoRepository {
+    
+    private List<Produto> produtos = new ArrayList<Produto>(); // Simula meu banco de dados para essa aplicação
+    private Integer ultimoId = 0;
+
+    /**
+     * Metodo para retornar uma lista de produtos
+     * @return Lista de produtos
+     */
+    public List<Produto> obterTodos(){
+        return produtos;
+    }
+
+    /**
+     * Metodo que retorna o produto encontrado pelo seu Id.
+     * @param id do produto que será localizado
+     * @return Retorna um produto caso tenha encontrado
+     */
+    public Optional<Produto> obterPorId(Integer id){
+        return produtos.stream()
+                      .filter(produto -> produto.getId() == id)
+                      .findFirst();
+    }
+
+    /**
+     * Metodo para adcionar produto na lista.
+     * @param produto que será adicionado na lista
+     * @return retorna o produto que foi adiiconado na lista
+     */
+    public Produto adicionar(Produto produto){
+        
+        ultimoId++;
+        produto.setId(ultimoId);
+        produtos.add(produto);
+        return produto;
+    }
+
+    /**
+     * Metodo para remover produto por id da lista.
+     * @param id do produto a ser removido.
+     */
+    public void remover(Integer id){
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    /**
+     * Metodo para atualizar o produto na lista
+     * @param produto que será atualizado
+     * @return Retorna o produto após a atualização na lista.
+     */
+    public Produto atualizar(Produto produto){
+        
+        //Encontra produto na lista.
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+
+        //Se produto encontrado, for vazio, ele retorna a mensagem.
+        if(produtoEncontrado.isEmpty()){
+            throw new InputMismatchException("Produto não encontrado");
+        }
+        //Remove produto antigo da lista.
+        remover(produto.getId());
+
+        //Depois adicionar o produto atualizado na lista.
+        produtos.add(produto);
+
+        return produto;
+    }
+}
