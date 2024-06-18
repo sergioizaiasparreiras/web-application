8fc955ea412e4233e8cfc95c5eefd715127b122b
sergioizaiasparreiras <sergioparreiras09@gmail.com>
2024-06-18 18:26:48 -0300

    fix: problama de conexao com o banco de dados e modificacao em servicos

--- codigos/back/src/main/java/com/safetrade/auth/services/ProdutoService.java
+++ codigos/back/src/main/java/com/safetrade/auth/services/ProdutoService.java
@@ -7,70 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.safetrade.auth.model.Produto;
-import com.safetrade.auth.repository.ProdutoRepository_old;
-
-
-
-/**
- *  Metodos de CRUD
- */
+import com.safetrade.auth.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
 
     @Autowired
-    private ProdutoRepository_old produtoRepository;
+    private ProdutoRepository produtoRepository;
 
-    /**
-     * Metodo para retornar uma lista de produtos
-     * @return list da produtos
-     */
     public List<Produto> obterTodos(){
-        return produtoRepository.obterTodos();
+        return produtoRepository.findAll();
     }
 
-    /**
-     * Metodo que retorna um produto encontrado por Id
-     * @param id que do produto que sera localizado
-     * @return retorna um produto que sera localizado
-     */
     public Optional<Produto> obterPorId(Integer id){
-
-        return produtoRepository.obterPorId(id);
+        return produtoRepository.findById(id);
     }
 
-    /**
-     * Metodo para adiconar produto na lista.
-     * @param produto que sera adicionado
-     * @return produto que foi adicionado a lista
-     */
     public Produto adicionar(Produto produto){
-        //Poderia ter algumas regras de negocio para validar o produto.
-        return produtoRepository.adicionar(produto); 
+        return produtoRepository.save(produto);
     }
 
-    /**
-     * Metodo para deletar o produto por id
-     * @param id do produto a ser deletado
-     */
     public void deletar(Integer id){
-        // Aqui poderia ter alguma lógica de validação.
-        produtoRepository.deletar(id);
+        produtoRepository.deleteById(id);
     }
 
-
-    /**
-     * Metodo para atualizar o produto na lista
-     * @param id na requisão de um método para atualizar,
-     * o id não vem no corpo do produto, ele vem separado.
-     * @param produto que será atualizado
-     * @return o produto após a atualização na lista
-     */
     public Produto atualizar(Integer id, Produto produto){
-
-        // Aqui poderia ter uma lógica de validação
         produto.setId(id);
-        return produtoRepository.atualizar(produto);
-
+        return produtoRepository.save(produto);
     }
 }
